a5ccad89ae4db24b7a7da008bc169a924e391730
Nicolas Marty <nicolas.marty@zuehlke.com>
2023-09-07 11:12:16 +0200

    update install script

--- install-aliases.sh
+++ install-aliases.sh
@@ -6,10 +6,9 @@ INSTALL_PATH=~/.gitaliases
 mkdir -p $INSTALL_PATH
 
 for alias in $ALL_ALIASES; do 
-  echo "install $alias"; 
-  cp $alias.sh $INSTALL_PATH/
   command="!sh ${INSTALL_PATH}/${alias}.sh"
-  echo $command
+  echo "install $alias - '$command'"
+  cp -f $alias.sh $INSTALL_PATH/
   git config --global --unset-all alias.$alias
   git config --global alias.$alias "${command}"
 done;
