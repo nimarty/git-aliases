0fbda1a653760ee2c228d7a3d7940af3e28a0022
Nicolas Marty <nicolas.marty@zuehlke.com>
2023-09-14 10:52:03 +0200

    add auto color to shortlog alias

--- aliases/shl.sh
+++ aliases/shl.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # alias to get a short log of the last n commits, 8 by default
-git log --pretty='format:%h %<(18)%an %d %s' -n ${1-8}
+git log --pretty='format:%C(auto)%h %<(18)%an %d %s' -n ${1-8}
